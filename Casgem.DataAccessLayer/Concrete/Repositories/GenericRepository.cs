f5fcaf4bdf2ce73ae908ddace4e2ebee80b2f24b
gokay <gokayacikgoz1@gmail.com>
2023-07-18 16:19:02 +0300

    Add project files.

---
+++ Casgem.DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Casgem.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem.DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T t)
+        {
+            _context.Remove(t);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            _context.Set<T>().Add(t);
+            _context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            _context.Set<T>().Update(t);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ CasgemApiLayer/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using Casgem.BusinessLayer.Abstract;
+using Casgem.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem.ApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddCategorty(Category category) 
+        {
+            _categoryService.TInsert(category);
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult DeleteCategorty(int id) 
+        {
+            var values = _categoryService.TGetById(id);
+            _categoryService.TDelete(values);
+            return Ok();
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpPut("update")]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _categoryService.TUpdate(category);
+            return Ok();
+        }
+    }
+}
